1aa2a6d7bffc4e10c641e714b2d2493268fff66b
Gabe <gabriel.gelgor@ontariotechu.net>
2020-08-21 11:48:30 -0400

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t gabrielgelgor/multi-client:latest -t gabrielgelgor/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t gabrielgelgor/mutli-server:latest -t gabrielgelgor/mutli-server:$SHA -f ./server/Dockerfile ./server
+docker build -t gabrielgelgor/multi-worker:latest -t gabrielgelgor/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push gabrielgelgor/multi-client:latest
+docker push gabrielgelgor/multi-server:latest
+docker push gabrielgelgor/multi-worker:latest
+
+docker push gabrielgelgor/multi-client:$SHA
+docker push gabrielgelgor/multi-server:$SHA
+docker push gabrielgelgor/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=gabrielgelgor/multi-server:$SHA
+kubectl set image deployments/client-deployment client=gabrielgelgor/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=gabrielgelgor/multi-worker:$SHA
